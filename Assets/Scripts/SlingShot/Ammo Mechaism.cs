071552bb0c6b2314fec1faad1feaefaf6c49eebd
datht4889 <hoangdat18112003@gmail.com>
2024-05-28 16:12:40 +0700

    Update Ammo Mechaism.cs

--- Assets/Scripts/SlingShot/Ammo Mechaism.cs
+++ Assets/Scripts/SlingShot/Ammo Mechaism.cs
@@ -8,9 +8,6 @@ public class AmmoMechaism : MonoBehaviour
     private Rigidbody2D rb;
     private CircleCollider2D circleCollider;
 
-    private EventTrigger eventTrigger;
-    private SpriteRenderer spriteRenderer;
-
     private bool isPowered = false;
     private bool isShooted;
     private bool shouldFaceVelDirection;
@@ -19,9 +16,6 @@ public class AmmoMechaism : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         circleCollider = GetComponent<CircleCollider2D>();
-        eventTrigger =  GetComponent<EventTrigger>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.drawMode = SpriteDrawMode.Sliced;
     }
 
     private void FixedUpdate()
@@ -52,19 +46,5 @@ public class AmmoMechaism : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         shouldFaceVelDirection = false;
-    }
-
-    private void OnMouseDown()
-    {
-        if (isPowered == false){
-            PowerUp();
-            isPowered = true;
-        }
-    }
-
-    private void PowerUp()
-    {
-        circleCollider.radius = 1.5f;
-        spriteRenderer.size = new Vector2(3, 3);
     }
 }
